09f3735bcdb8037fd04f3adda864f6d00e478fce
Wxz <429373960@qq.com>
2022-11-15 18:08:03 +0800

    update shader

---
+++ Shaders/gbuffer_ps.hlsl
@@ -0,0 +1,4 @@
+float4 main() : SV_TARGET
+{
+	return float4(0.0f, 0.57f, 0.21f, 1.0f);
+}
---
+++ Tools/ShaderCompiler/ShaderCompiler.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <filesystem>
+#include <cstdlib>
+enum class ShaderType
+{
+	Vertex,
+	Pixel
+};
+
+void CompileShader(const char* path, const char* entry, ShaderType type)
+{
+	std::string dxcPath = std::string(PROJECT_PATH) + "/ThirdParty/DirectXShaderCompiler/bin/x64/dxc.exe";
+	std::string shaderPath = std::string(PROJECT_PATH) + "/Shaders/" + std::string(path);
+	std::string entryName = entry;
+	std::string targerName;
+	if (type == ShaderType::Vertex)
+	{
+		targerName = "vs_6_6";
+	}
+	else if (type == ShaderType::Pixel)
+	{
+		targerName = "ps_6_6";
+	}
+
+	std::filesystem::path myPath{ path };
+	auto myPathStem = myPath.stem();
+	std::string headerName = myPathStem.string() + ".h";
+	std::string headrPath = std::string(PROJECT_PATH) + "/Include/DQ/Shaders/" + headerName;
+	std::string str = dxcPath + " " + shaderPath + " /E " + entryName + " /T " + targerName + " /Fh " + headrPath + " /Vn " + myPathStem.string();
+	system(str.c_str());
+}
+
+int main()
+{
+	CompileShader("gbuffer_vs.hlsl", "main", ShaderType::Vertex);
+	CompileShader("gbuffer_ps.hlsl", "main", ShaderType::Pixel);
+}
