44aa89ed96e30760b3df09f24cc26c46a7997226
Wxz <429373960@qq.com>
2022-11-28 01:35:27 +0800

    class Scene

--- Include/DQ/Scene/Scene.h
+++ Include/DQ/Scene/Scene.h
@@ -1,10 +1,13 @@
 #pragma once
-#include "Model.hpp"
+#include <memory>
 namespace DQ
 {
     class IScene
     {
     public:
         virtual void LoadModel(const char* path) = 0;
+        //virtual void ExecuteUpdate(float )
     };
+
+    std::shared_ptr<IScene> CreateScene();
 }
--- Scene/Scene.cpp
+++ Scene/Scene.cpp
@@ -1,1 +1,17 @@
 #include <DQ/Scene/Scene.h>
+namespace DQ
+{
+    class Scene : public IScene
+    {
+    public:
+        void LoadModel(const char* path)
+        {
+
+        }
+    };
+
+    std::shared_ptr<IScene> CreateScene()
+    {
+        return std::shared_ptr<IScene>(new Scene);
+    }
+}
