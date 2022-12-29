4afda35bbf566ecb7ae5caaebcebc7a3e9336965
Wxz <429373960@qq.com>
2022-12-30 07:27:41 +0800

    update

--- Examples/HelloWindow/HelloWindow.cpp
+++ Examples/HelloWindow/HelloWindow.cpp
@@ -1,10 +1,41 @@
 #include <Window/Window.h>
+#include <Component/Component.hpp>
+#include <iostream>
+
+void DrawVec(const glm::mat4 &v)
+{
+    std::cout << v[0][0] << " " << v[0][1] << " " << v[0][2] << " " << v[0][3] << " " << std::endl;
+    std::cout << v[1][0] << " " << v[1][1] << " " << v[1][2] << " " << v[1][3] << " " << std::endl;
+    std::cout << v[2][0] << " " << v[2][1] << " " << v[2][2] << " " << v[2][3] << " " << std::endl;
+    std::cout << v[3][0] << " " << v[3][1] << " " << v[3][2] << " " << v[3][3] << " " << std::endl;
+    std::cout << std::endl;
+}
+void DrawVec(const DirectX::XMFLOAT4X4& v)
+{
+    std::cout << v.m[0][0] << " " << v.m[0][1] << " " << v.m[0][2] << " " << v.m[0][3] << " " << std::endl;
+    std::cout << v.m[1][0] << " " << v.m[1][1] << " " << v.m[1][2] << " " << v.m[1][3] << " " << std::endl;
+    std::cout << v.m[2][0] << " " << v.m[2][1] << " " << v.m[2][2] << " " << v.m[2][3] << " " << std::endl;
+    std::cout << v.m[3][0] << " " << v.m[3][1] << " " << v.m[3][2] << " " << v.m[3][3] << " " << std::endl;
+    std::cout << std::endl;
+}
+
 
 class Window : public DQ::IApp
 {
 public:
     bool Init()
     {
+        DQ::CameraComponent ss;
+        AllocConsole();
+        freopen("CONOUT$", "w", stdout);
+        DrawVec(ss.m);
+        DrawVec(ss.v);
+        DrawVec(ss.p);
+        DrawVec(ss._m);
+        DrawVec(ss._v);
+        DrawVec(ss._p);
+        DrawVec(ss.p * ss.v);
+        DrawVec(ss._vp);
         return true;
     }
 
--- Include/Component/Component.hpp
+++ Include/Component/Component.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include "../glm/glm.hpp"
 #include "../glm/ext/matrix_transform.hpp"
+#include "../glm/ext/matrix_clip_space.hpp"
+#include <cstdint>
 namespace DQ
 {
     class Componet
@@ -15,17 +17,22 @@ namespace DQ
     public:
         CameraComponent()
         {
-            m = glm::mat4(1.0f);
+            w = 800;
+            h = 600;
 
-            pos = glm::vec3(0.f, 0.f, 0.f);
+            pos = glm::vec3(22.f, 0.f, 0.f);
             dir = glm::vec3(0.f, 0.f, 1.f);
             up = glm::vec3(0.f, 1.f, 0.f);
-
-            v = glm::lookAtLH(pos, dir, up);
-            
+            m = glm::mat4(1.0f);
+            v = glm::lookAtLH(pos, pos + dir, up);
+            p = glm::perspectiveLH_ZO(glm::radians(45.f), (float)w / (float)h, 0.1f, 1000.0f);
         }
 
-    private:
+    //private:
+
+        uint32_t w;
+        uint32_t h;
+
         glm::vec3 pos;
         glm::vec3 dir;
         glm::vec3 up;
